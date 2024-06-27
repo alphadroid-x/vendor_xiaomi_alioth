8e2e206973bb7c8eb293d900255b6955320b6386
Gustavo Mendes <gusttavo.me@outlook.com>
2024-06-27 05:24:05 -0300

    alioth: Sync with proprietary-files - Declare EGL libs as symlinks during extraction. - Mark CNE symlink target as required package.
    Change-Id: I896d00e4faed50e0a9d1180e426137efb463a716
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- alioth-vendor.mk
+++ alioth-vendor.mk
@@ -1216,3 +1216,11 @@ PRODUCT_PACKAGES += \
     manifest_vendor.xiaomi.hardware.mlipay \
     manifest_vendor.xiaomi.hardware.mtdservice \
     vendor.qti.gnss@4.0-service
+
+PRODUCT_PACKAGES += \
+    libEGL_adreno_libEGL_adreno_symlink32 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink32 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink32 \
+    libEGL_adreno_libEGL_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64
